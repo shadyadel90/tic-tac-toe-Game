7b44de7982447beb09e9b23d91d1dbb830e7afe7
shadyadel90 <31343078+shadyadel90@users.noreply.github.com>
2024-01-26 09:42:02 +0200

    project intialized added app icon added launch screen renamed app entry renamed content view to start view

---
+++ XandO/XandO/AppEntry.swift
@@ -0,0 +1,17 @@
+//
+//  XandOApp.swift
+//  XandO
+//
+//  Created by Shady Adel on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppEntry: App {
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
